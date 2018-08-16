a158d596e3ca7552e3d78bd2ba5ab0793d5ee8b2
suihan <suihan742@gmail.com>
2018-08-16 17:15:28 +0900

    Added Trends

---
+++ Mastonet/MastodonClient.Trends.cs
@@ -0,0 +1,21 @@
+﻿using Mastonet.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mastonet
+{
+    partial class MastodonClient
+    {
+        /// <summary>
+        /// Retrieving trending hashtags
+        /// </summary>
+        /// <param name="statusId"></param>
+        /// <returns>Returns a Status</returns>
+        public Task<MastodonList<Tag>> GetTrends()
+        {
+            return GetList<Tag>($"/api/v1/trends");
+        }
+    }
+}
